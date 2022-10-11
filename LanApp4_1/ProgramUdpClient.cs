b42b7ccd73975a5bf256d29f97f6a6a7a3bdd92a
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-10-11 20:28:00 +0300

    Lesson 4-1

---
+++ LanApp4_1/ProgramUdpClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LanApp4_1
+{
+    class ProgramUdpClient
+    {
+        static string remoteAddress; // адрес получателя
+        static int remotePort; // порт получателя
+        static int localPort; // порт приема
+        static void Main(string[] args)
+        {
+            Console.Title = "UDPClient Test ";
+            try
+            {
+                Console.Write("Local port: ");
+                localPort = int.Parse(Console.ReadLine());
+                Console.Write("Remote port: ");
+                remotePort = int.Parse(Console.ReadLine());
+                Console.Write("Remote address: ");
+                remoteAddress = Console.ReadLine();
+
+                var taskReceiver = Task.Run(ReceiveMessage);
+
+                SendMessage();
+                //Task.WaitAll(taskReceiver);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void SendMessage()
+        {
+            UdpClient client = new UdpClient();
+            string message;
+            byte[] buffer;
+            int bytes;
+            Console.WriteLine("Enter your message: ");
+            try
+            {
+                while (true)
+                {
+                    message = Console.ReadLine();
+                    if (message.ToLower().Equals("exit"))
+                        break;
+                    else
+                    {
+                        buffer = Encoding.UTF8.GetBytes(message);
+                        bytes = client.Send(buffer, buffer.Length, remoteAddress, remotePort);
+                        if (buffer.Length != bytes)
+                        {
+                            Console.WriteLine($"Warning!!! Send {bytes} from {buffer.Length} bytes!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+            
+        }
+        static void ReceiveMessage()
+        {
+            UdpClient server = new UdpClient(localPort);
+            IPEndPoint endPoint = null;
+            try
+            {
+                byte[] buffer;
+                string message;
+                while (true)
+                {
+                    buffer = server.Receive(ref endPoint);
+                    message = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                    Console.WriteLine($"{endPoint} >> {message}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Close();
+            }
+        }
+    }
+}
---
+++ LanApp4_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LanApp4_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LanApp4_1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b118d4a6-4724-4aeb-8682-4eef2e303da3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
